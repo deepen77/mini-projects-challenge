e91e280a475e2207a6399e6adf4be100d084040f
deepen77 <przemek.starostka@yahoo.com>
2021-06-07 19:00:07 +0100

    001-expanding_cards

---
+++ 001 - Expanding Cards/index.js
@@ -0,0 +1,16 @@
+const cards = document.querySelectorAll('.card')
+
+
+
+cards.forEach((card) => {
+    card.addEventListener('click', () => {
+        removeActiveClasses();
+        card.classList.add('active')
+    })
+})
+
+const removeActiveClasses = () => {
+    cards.forEach( card => {
+        card.classList.remove('active')
+    })
+}
